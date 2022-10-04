1891782fa2d2ddfb11d9d519f55bf1637ccc9738
Test User <javidx24@icloud.com>
2022-10-04 19:50:56 -0400

    error page added

--- src/Pages/ErrorPage/ErrorPage.jsx
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -1,9 +1,18 @@
 import "./ErrorPage.css"
 import React from 'react'
+import errorIcon from "../../Images/error-icon.png"
+import { useNavigate } from "react-router-dom"
 
 const ErrorPage = () => {
+  const navigate = useNavigate();
   return (
-    <div>ErrorPage</div>
+    <div>
+      <div className="error-page-container">
+        <img src={errorIcon} alt="error-icon"/>
+        <h1>Page not found</h1>
+        <a onClick={() => navigate('/home')}>return to home?</a>
+      </div>
+    </div>
   )
 }
 
