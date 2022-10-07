d36d929769b1afe8857b4362042184ff463774a2
Test User <javidx24@icloud.com>
2022-10-06 20:23:37 -0400

    adding drop menu

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,28 +2,36 @@ import React from 'react'
 import "./Header.css"
 import { Link, useNavigate } from 'react-router-dom';
 import mergeLogo from "../../Images/mergeLogo.png"
+import { HiMenuAlt3 } from 'react-icons/hi'
+import { useState } from 'react';
 
 const Header = () => {
+
+  const [ isOpen, setIsOpen ] = useState(false)
+  
   const navigate = useNavigate();
   return (
     <div>
       <header>
-        <div className="left-side-logo">
-          <img src ={mergeLogo} alt="merge-logo"/>
+        <div className="left-side-logo"  >
+          <img src={mergeLogo} alt="merge-logo" />
           <h3>MERGE</h3>
         </div>
-        <div className="middle">
+        <div className="middle" id={isOpen ? "hidden" : ""} >
           {/* ~a~ TAG IN CSS WORKS ON ~Link~ */}
-          <Link to="/solutions">SOLUTIONS</Link>
-          <Link to="/demo">DEMO</Link>
-          <Link to="/products">PRODUCTS</Link>
-          <Link to="/pricing">PRICING</Link>
-          <Link to="/resources">RESOURCES</Link>
+          <Link onClick={() => setIsOpen(!isOpen)} to="/solutions">SOLUTIONS</Link>
+          <Link onClick={() => setIsOpen(!isOpen)} to="/demo">DEMO</Link>
+          <Link onClick={() => setIsOpen(!isOpen)} to="/products">PRODUCTS</Link>
+          <Link onClick={() => setIsOpen(!isOpen)} to="/pricing">PRICING</Link>
+          <Link onClick={() => setIsOpen(!isOpen)} to="/resources">RESOURCES</Link>
         </div>
-        <div className="right-side">
+        <div className="right-side" >
           <button className="register" onClick={() => navigate('/register')}>REGISTER</button>
           <button className="sign-up" onClick={() => navigate('/home')}>HOME</button>
           {/* <button className="sign-up">SIGN IN</button> */}
+          <div className="hamburger-menu" id="hamburger">
+            <HiMenuAlt3 size={50} onClick={() => setIsOpen(!isOpen)} />
+          </div>
         </div>
       </header>
     </div>
