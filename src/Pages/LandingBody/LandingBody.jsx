828e662e6e756f6de0a71d645f47ebdf7e46a70c
Test User <javidx24@icloud.com>
2022-10-12 20:13:40 -0400

    edits to the landing page emailinput

--- src/Pages/LandingBody/LandingBody.jsx
+++ src/Pages/LandingBody/LandingBody.jsx
@@ -84,10 +84,10 @@ const LandingBody = () => {
           <p className="provides">
             <span>👌</span>  Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, quas.
           </p>
-          <div className="email-enter">
+          <div className="email-wrapper">
               <form className="form-container-2" >
                 <input name="email" placeholder="Business Email" value={inputData.email} onChange={onChange} required />
-                <button className="button-1" onClick={(e) => handleSubmit(e)}>Join Merge!</button></form>
+                <button onClick={(e) => handleSubmit(e)}>Join Merge</button></form>
           </div>
         </div>
         <div className="hero-img">
