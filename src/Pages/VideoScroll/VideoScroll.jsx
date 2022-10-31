f053c9f5d3388704feeca87422790c34f1bdf4be
David_Martinez <javidx24@icloud.com>
2022-10-31 16:55:16 -0400

    edits to sites responsiveness

--- src/Pages/VideoScroll/VideoScroll.jsx
+++ src/Pages/VideoScroll/VideoScroll.jsx
@@ -8,6 +8,7 @@ import img1 from "../../Images/img_1.png"
 import img2 from "../../Images/img_2.png"
 import img3 from "../../Images/img_3.png"
 import img4 from "../../Images/img_4.png"
+import videoGif from "../../Images/videoScrollGif.gif"
 gsap.registerPlugin(ScrollTrigger);
 gsap.registerPlugin(ScrollToPlugin);
 
@@ -19,41 +20,48 @@ const VideoScroll = () => {
   const videoRef = useRef(null);
   const enjoyRef = useRef(null)
 
+
   useEffect(() => {
     videoRef.current.currentTime = 0;
-mm.add('(min-width: 767px)', () =>{
-    ScrollTrigger.create({
-      trigger: videoRef.current,
-      scrub: .5,
-      pin: videoRef.current,
-      start: 'center center',
-      end: '+=4000',
-      // markers: true,
-      onUpdate: (self) => {
-        if (videoRef.current) {
-          let scrollPos = self.progress;
-          let videoDuration = videoRef.current.duration;
-          let videoCurrentTime = videoDuration * scrollPos;
-          if (videoCurrentTime) {
-            videoRef.current.currentTime = videoCurrentTime;
+    mm.add('(min-width: 767px)', () => {
+      //! MAKES THE GIF FOR MOBILE DEVICES HIDDEN
+      gsap.set(".videoGif", { display: "none" })
+      gsap.set(".p-gif", { display: "none" })
+      ScrollTrigger.create({
+        trigger: videoRef.current,
+        scrub: .5,
+        pin: videoRef.current,
+        start: 'center center',
+        end: '+=4000',
+        pinSpacing: true,
+        // markers: true,
+        // id: "trigger",
+        onUpdate: (self) => {
+          if (videoRef.current) {
+            let scrollPos = self.progress;
+            let videoDuration = videoRef.current.duration;
+            let videoCurrentTime = videoDuration * scrollPos;
+            if (videoCurrentTime) {
+              videoRef.current.currentTime = videoCurrentTime;
+            }
           }
-        }
-      },
+        },
+      })
     })
-  })
   }, [IntroVideoRef, videoRef]);
 
   useEffect(() => {
-    mm.add('(min-width: 767px)', () =>{
-    ScrollTrigger.create({
-      trigger: enjoyRef.current,
-      scrub: .5,
-      pin: enjoyRef.current,
-      start: 'center center',
-      end: '+=1000',
-      // markers: true
+    mm.add('(min-width: 767px)', () => {
+      ScrollTrigger.create({
+        trigger: enjoyRef.current,
+        scrub: .5,
+        pin: enjoyRef.current,
+        start: 'center center',
+        end: '+=1000',
+        // markers: true,
+        // id: "enjoy",
+      })
     })
-  })
   }, [])
 
   //----------------------------------//----------------------------------//----------------------------------
@@ -63,88 +71,93 @@ mm.add('(min-width: 767px)', () =>{
   const someRef = useRef(null)
 
   useEffect(() => {
-    let points = gsap.utils.toArray('.point');
-    let anchors = gsap.utils.toArray('.tabs a');
-    console.log(anchors)
+    mm.add('(min-width: 767px)', () => {
+      //! CHANGES THE POSITION FOR MOBILE DEVICES
+      gsap.set(".tabs", { display: "flex" })
+      gsap.set(".philosophie", { height: "auto" })
 
-    //? THIS DICTATES HOW LONG EACH `.point` LAST ON SCROLL 
-    let height = 200 * points.length;
-    console.table(height)
+      let points = gsap.utils.toArray('.point');
+      let anchors = gsap.utils.toArray('.tabs a');
+      console.log(anchors)
+
+      //? THIS DICTATES HOW LONG EACH `.point` LAST ON SCROLL 
+      let height = 200 * points.length;
+      console.table(height)
 
 
-    let tl = gsap.timeline({
-      duration: points.length,
-      scrollTrigger: {
+      let tl = gsap.timeline({
+        duration: points.length,
+        scrollTrigger: {
+          trigger: philoRef.current,
+          start: "top center",
+          end: "+=" + height + "%",
+          scrub: true,
+          markers: false
+        }
+
+      })
+
+
+      //? THIS MAKES THE ELEMENT ACTUALLY PIN
+      ScrollTrigger.create({
         trigger: philoRef.current,
-        start: "top center",
+        start: "top top",
         end: "+=" + height + "%",
         scrub: true,
-        markers: false
-      }
+        pin: philoRef.current,
+        id: "#sec3",
 
+      })
+
+
+      console.log(points)
+
+      //? CYCLES THROUGH EACH .point in POINTS ARRAY
+      points.forEach(function (elem, i) {
+
+        //? this line is setting every ".point" (which is looped as "elem") to those css properties
+        gsap.set(elem, { position: "absolute", top: 0 });
+
+
+        //? THIS LINE IS THE TIMES COMING UP ON THE SCREEN
+        tl.to(anchors[i], { color: "orange", duration: 0.25 }, i)
+        tl.from(elem.querySelector('img'), { autoAlpha: 0 }, i)
+        tl.from(elem.querySelector('article'), { autoAlpha: 0, translateY: 100 }, i)
+
+        //? THIS LINE IS THE ELEMENTS LEAVING THE SCREEN, ADDING .75 TO THE INDEX TILL THE INDEX IS ABOVE 4
+        if (i != points.length - 1) {
+          tl.to(anchors[i], { color: "black", duration: 0.25 }, i + 0.75)
+          tl.to(elem.querySelector('article'), { autoAlpha: 0, translateY: -100 }, i + 0.75)
+          tl.to(elem.querySelector('img'), { autoAlpha: 0 }, i + 0.75)
+        }
+
+
+      });
     })
 
-
-    //? THIS MAKES THE ELEMENT ACTUALLY PIN
-    ScrollTrigger.create({
-      trigger: philoRef.current,
-      start: "top top",
-      end: "+=" + height + "%",
-      scrub: true,
-      pin: philoRef.current,
-      id: "#sec3",
-
-    })
-
-
-    console.log(points)
-
-    //? CYCLES THROUGH EACH .point in POINTS ARRAY
-    points.forEach(function (elem, i) {
-
-      //? this line is setting every ".point" (which is looped as "elem") to those css properties
-      gsap.set(elem, { position: "absolute", top: 0 });
-
-
-      //? THIS LINE IS THE TIMES COMING UP ON THE SCREEN
-      tl.to(anchors[i], {color: "orange", duration: 0.25}, i)
-      tl.from(elem.querySelector('img'), { autoAlpha: 0 }, i)
-      tl.from(elem.querySelector('article'), { autoAlpha: 0, translateY: 100 }, i)
-
-      //? THIS LINE IS THE ELEMENTS LEAVING THE SCREEN, ADDING .75 TO THE INDEX TILL THE INDEX IS ABOVE 4
-      if (i != points.length - 1) {
-        tl.to(anchors[i], {color: "black", duration: 0.25}, i+0.75)
-        tl.to(elem.querySelector('article'), { autoAlpha: 0, translateY: -100 }, i + 0.75)
-        tl.to(elem.querySelector('img'), { autoAlpha: 0 }, i + 0.75)
-      }
-
-      
-    });
-
-    
   }, [philoRef])
   const distance = 5000;
 
   const handleSec1 = (e) => {
     e.preventDefault()
-    gsap.to(window, {duration: 1, scrollTo: distance});
+    gsap.to(window, { duration: 1, scrollTo: distance });
   }
- 
+
   const handleSec2 = (e) => {
     e.preventDefault()
-    gsap.to(window, {duration: 1, scrollTo: 7000});
+    gsap.to(window, { duration: 1, scrollTo: 7000 });
   }
- 
+
   const handleSec3 = (e) => {
     e.preventDefault()
-    gsap.to(window, {duration: 1, scrollTo: 9000});
+    gsap.to(window, { duration: 1, scrollTo: 9000 });
   }
- 
+
   const handleSec4 = (e) => {
     e.preventDefault()
-    gsap.to(window, {duration: 1, scrollTo: 11000});
+    gsap.to(window, { duration: 1, scrollTo: 11000 });
   }
- 
+
   return (
     <>
       <div className="scroll-container">
@@ -153,11 +166,13 @@ mm.add('(min-width: 767px)', () =>{
           <h1 className='left-text-2' ref={enjoyRef}>Enjoy!</h1>
         </div>
         <div className="section-right">
-          <video autoPlay loop id="video" className="vid" ref={videoRef} src={scrollVideo}></video>
+          <video id="video" className="vid" ref={videoRef} src={scrollVideo}></video>
+          <p className="p-gif">(this is the mobile version using a Gif instead of an MP4)</p>
+          <img className="videoGif" src={videoGif} alt="video-gif" />
         </div>
       </div>
 
-<div className='spacer'></div>
+      <div className='spacer'></div>
 
       <section ref={philoRef} className="philosophie">
         <div ref={wrapperRef} className="wrapper">
@@ -203,7 +218,7 @@ mm.add('(min-width: 767px)', () =>{
         </div>
 
       </section>
-     
+   
 
       {/* <div className="scroll-section-wrapper">
                 <div className="left-scroll-section-text">
