6db096f039eacde19fb2ab398329f6fcfd4a8c09
David_Martinez <javidx24@icloud.com>
2022-11-01 16:51:00 -0400

    edits to UI

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -15,20 +15,22 @@ const Header = () => {
     <div>
       <header>
         <div className="left-side-logo"  >
-          <img src={mergeLogo} alt="merge-logo" />
-          <h3>MERGE</h3>
+          <a href="/home" className="home-logo">
+            <img src={mergeLogo} alt="merge-logo" />
+            <h3>MERGE</h3>
+          </a>
         </div>
         <div className="middle" id={isOpen ? "hidden" : ""} >
           {/* ~a~ TAG IN CSS WORKS ON ~Link~ */}
-          
+
           <Link onClick={() => setIsOpen(!isOpen)} to="/videoscroll">DEMO</Link>
-          <Link onClick={() => setIsOpen(!isOpen)} to="/products"><span>PRODUCTS </span><svg width="15" height="12" viewBox="0 0 11 7" fill="none" xmlns="http://www.w3.org/2000/svg" transform="rotate(180)"><path d="M9.61914 5.42871L5.77152 1.53977L1.9239 5.42871" stroke="currentColor" stroke-width="1.92381" stroke-linecap="round" stroke-linejoin="round"></path></svg></Link>
+          <Link onClick={() => setIsOpen(!isOpen)} to="/products">PRODUCTS </Link>
           <Link onClick={() => setIsOpen(!isOpen)} to="/pricing">PRICING </Link>
           <Link onClick={() => setIsOpen(!isOpen)} to="/resources">RESOURCES</Link>
         </div>
         <div className="right-side" >
           <button className="register" onClick={() => navigate('/register')}>REGISTER</button>
-          <button className="sign-up" onClick={() => navigate('/home')}>HOME</button>
+          <button className="sign-up" onClick={() => navigate('/home')}>SIGN IN</button>
           {/* <button className="sign-up">SIGN IN</button> */}
           <div className="hamburger-menu" id="hamburger">
             <HiMenuAlt3 size={50} onClick={() => setIsOpen(!isOpen)} />
